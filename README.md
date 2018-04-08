ae24cc2eb04a12b0d27e68aa6918e00e4f5ebd57
jacyhowe <jacyhowe@hotmail.com>
2018-04-07 18:50:24 -0600

    update readme and python script

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # My Git Project
 
->My code for the git project
+> My innocent change
 
 ## I'm in the featured branch
 
--- my_file.py
+++ my_file.py
@@ -7,7 +7,4 @@ def hi_there ():
 
     print("Hi Git")
 
-print("Hi there from another dev")
-
-def hi_there ():
-    print("Hi")
+    print("My local change")
